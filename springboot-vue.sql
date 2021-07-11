4a0a315e2c13bae19f3a247ceffe3a7a80264c82
xqnode <xq1993916>
2021-07-11 08:28:41 +0800

    文件上传

--- springboot-vue.sql
+++ springboot-vue.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50721
  File Encoding         : 65001
 
- Date: 08/07/2021 17:10:04
+ Date: 11/07/2021 08:27:54
 */
 
 SET NAMES utf8mb4;
@@ -27,13 +27,14 @@ CREATE TABLE `book`  (
   `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '价格',
   `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '作者',
   `create_time` datetime NULL DEFAULT NULL COMMENT '出版日期',
+  `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '封面地址',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of book
 -- ----------------------------
-INSERT INTO `book` VALUES (1, '明朝那些事', 60.00, 'xxx', '2010-07-01 08:00:00');
+INSERT INTO `book` VALUES (1, '明朝那些事', 60.00, 'xxx', '2010-07-01 00:00:00', 'http://localhost:9090/files/0160d55feff34adeb3bdb4939892546c');
 
 -- ----------------------------
 -- Table structure for user
