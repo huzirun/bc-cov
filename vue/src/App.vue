7746fe4e3f3b079b0b938d286d490f6e902c8fc0
xqnode <xq1993916>
2021-07-27 17:33:45 +0800

    升级vue和element-plus版本到最新版本，同时修改国际化配置

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
+  <el-config-provider :locale="locale">
     <router-view />
-  </div>
+  </el-config-provider>
 
 </template>
 
 
 <script>
+import { ElConfigProvider } from 'element-plus'
+
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
 export default {
   name: "App",
+  components: {
+    [ElConfigProvider.name]: ElConfigProvider,
+  },
+  data() {
+    return {
+      locale: zhCn,
+    }
+  },
 }
 </script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -4,9 +4,9 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
-import 'dayjs/locale/zh-cn'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
+// import 'dayjs/locale/zh-cn'
+// import locale from 'element-plus/lib/locale/lang/zh-cn'
 
 import '@/assets/css/global.css'
 
-createApp(App).use(store).use(router).use(ElementPlus, { locale, size: 'small' }).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus, {size: 'small' }).mount('#app')
