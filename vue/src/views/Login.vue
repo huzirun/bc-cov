0f2fe88961391638fa64e0e0329c80f4e3470e1e
xqnode <xq1993916>
2021-07-29 14:20:50 +0800

    背景图片

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
-  <div style="width: 100%; height: 100vh;background-color: darkslateblue; overflow: hidden">
+  <div style="width: 100%; height: 100vh; overflow: hidden"> <!--  :style="bg" 加背景图片-->
     <div style="width: 400px; margin: 100px auto">
-      <div style="color: #cccccc; font-size: 30px; text-align: center; padding: 30px 0">欢迎登录</div>
+      <div style="font-size: 30px; text-align: center; padding: 30px 0">欢迎登录</div>
       <el-form ref="form" :model="form" size="normal" :rules="rules">
         <el-form-item prop="username">
           <el-input prefix-icon="el-icon-user-solid" v-model="form.username"></el-input>
@@ -10,8 +10,8 @@
           <el-input prefix-icon="el-icon-lock" v-model="form.password" show-password></el-input>
         </el-form-item>
         <el-form-item>
-          <el-radio v-model="form.role" :label="1" style="color: #eee">管理员</el-radio>
-          <el-radio v-model="form.role" :label="2" style="color: #eee">普通用户</el-radio>
+          <el-radio v-model="form.role" :label="1">管理员</el-radio>
+          <el-radio v-model="form.role" :label="2">普通用户</el-radio>
         </el-form-item>
         <el-form-item>
           <el-button style="width: 100%" type="primary" @click="login">登 录</el-button>
@@ -36,7 +36,13 @@ export default {
         password: [
           {required: true, message: '请输入密码', trigger: 'blur'},
         ],
-      }
+      },
+      // 加背景图片
+      // bg: {
+      //   backgroundImage: "url(" + require("@/assets/bg.jpg") + ")",
+      //   backgroundRepeat: "no-repeat",
+      //   backgroundSize: "100% 100%"
+      // }
     }
   },
   created() {
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,7 +1,7 @@
 <template>
-  <div style="width: 100%; height: 100vh;background-color: darkslateblue; overflow: hidden">
+  <div style="width: 100%; height: 100vh; overflow: hidden">
     <div style="width: 400px; margin: 100px auto">
-      <div style="color: #cccccc; font-size: 30px; text-align: center; padding: 30px 0">欢迎注册</div>
+      <div style="font-size: 30px; text-align: center; padding: 30px 0">欢迎注册</div>
       <el-form ref="form" :model="form" size="normal" :rules="rules">
         <el-form-item prop="username">
           <el-input prefix-icon="el-icon-user-solid" v-model="form.username"></el-input>
