7349c975306589b3efc4586604a799816a15ae68
xqnode <xqnode@163.com>
2021-11-01 14:06:12 +0800

    视频动态背景

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,31 +1,40 @@
 <template>
-  <div style="width: 100%; height: 100vh; overflow: hidden"> <!--  :style="bg" 加背景图片-->
-    <div style="width: 400px; margin: 100px auto">
-      <div style="font-size: 30px; text-align: center; padding: 30px 0">欢迎登录</div>
-      <el-form ref="form" :model="form" size="normal" :rules="rules">
-        <el-form-item prop="username">
-          <el-input prefix-icon="el-icon-user-solid" v-model="form.username" placeholder="请输入账号"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input prefix-icon="el-icon-lock" v-model="form.password" show-password placeholder="请输入密码"></el-input>
-        </el-form-item>
-        <el-form-item>
-         <div style="display: flex">
-           <el-input prefix-icon="el-icon-key" v-model="form.validCode" style="width: 50%;" placeholder="请输入验证码"></el-input>
-           <ValidCode @input="createValidCode" />
-         </div>
-        </el-form-item>
-        <el-form-item>
-          <el-radio v-model="form.role" :label="1">管理员</el-radio>
-          <el-radio v-model="form.role" :label="2">普通用户</el-radio>
-        </el-form-item>
-        <el-form-item>
-          <el-button style="width: 100%" type="primary" @click="login">登 录</el-button>
-        </el-form-item>
-        <el-form-item><el-button type="text" @click="$router.push('/register')">前往注册 >> </el-button></el-form-item>
-      </el-form>
+  <div class="homepage-hero-module">
+    <div class="video-container">
+      <div :style="fixStyle" class="filter">
+        <div style="width: 400px; margin: 100px auto">
+          <div style="font-size: 30px; text-align: center; padding: 30px 0; color: #333">欢迎登录</div>
+          <el-form ref="form" :model="form" size="normal" :rules="rules">
+            <el-form-item prop="username">
+              <el-input prefix-icon="el-icon-user-solid" v-model="form.username" placeholder="请输入账号"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input prefix-icon="el-icon-lock" v-model="form.password" show-password placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <div style="display: flex">
+                <el-input prefix-icon="el-icon-key" v-model="form.validCode" style="width: 50%;" placeholder="请输入验证码"></el-input>
+                <ValidCode @input="createValidCode" />
+              </div>
+            </el-form-item>
+            <el-form-item>
+              <el-radio v-model="form.role" :label="1" style="color: white">管理员</el-radio>
+              <el-radio v-model="form.role" :label="2" style="color: white">普通用户</el-radio>
+            </el-form-item>
+            <el-form-item>
+              <el-button style="width: 100%" type="primary" @click="login">登 录</el-button>
+            </el-form-item>
+            <el-form-item><el-button type="text" @click="$router.push('/register')">前往注册 >> </el-button></el-form-item>
+          </el-form>
+        </div>
+      </div>
+      <video :style="fixStyle" autoplay loop muted class="fillWidth" v-on:canplay="canplay">
+        <source src="../assets/sea.mp4" type="video/mp4"/>
+        浏览器不支持 video 标签，建议升级浏览器。
+      </video>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -35,7 +44,9 @@ import ValidCode from "@/components/ValidCode";
 export default {
   name: "Login",
   components: {
-    ValidCode
+    ValidCode,
+    vedioCanPlay: false,
+    fixStyle: '',
   },
   data() {
     return {
@@ -57,8 +68,36 @@ export default {
       // }
     }
   },
-  created() {
+  mounted() {
     sessionStorage.removeItem("user")
+
+    window.onresize = () => {
+      const windowWidth = document.body.clientWidth
+      const windowHeight = document.body.clientHeight
+      const windowAspectRatio = windowHeight / windowWidth
+      let videoWidth
+      let videoHeight
+      if (windowAspectRatio < 0.5625) {
+        videoWidth = windowWidth
+        videoHeight = videoWidth * 0.5625
+        this.fixStyle = {
+          height: windowWidth * 0.5625 + 'px',
+          width: windowWidth + 'px',
+          'margin-bottom': (windowHeight - videoHeight) / 2 + 'px',
+          'margin-left': 'initial'
+        }
+      } else {
+        videoHeight = windowHeight
+        videoWidth = videoHeight / 0.5625
+        this.fixStyle = {
+          height: windowHeight + 'px',
+          width: windowHeight / 0.5625 + 'px',
+          'margin-left': (windowWidth - videoWidth) / 2 + 'px',
+          'margin-bottom': 'initial'
+        }
+      }
+    }
+    window.onresize()
   },
   methods: {
     // 接收验证码组件提交的 4位验证码
@@ -99,5 +138,26 @@ export default {
 </script>
 
 <style scoped>
+.homepage-hero-module,
+.video-container {
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+}
 
+.video-container .poster img{
+  z-index: 0;
+  position: absolute;
+}
+
+.video-container .filter {
+  z-index: 1;
+  position: absolute;
+  /*background: rgba(0, 0, 0, 0.4);*/
+  width: 100%;
+}
+
+.fillWidth {
+  width: 100%;
+}
 </style>
