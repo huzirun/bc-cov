9974757caba89d81cc0f190e6d050ef7fc28f20b
xqnode <xqnode@163.com>
2021-11-04 07:45:00 +0800

    图标

--- vue/src/components/Aside.vue
+++ vue/src/components/Aside.vue
@@ -5,19 +5,39 @@
         :default-active="path"
         router
     >
-      <el-menu-item index="/home">主页</el-menu-item>
-      <el-menu-item index="/category">分类管理</el-menu-item>
-      <el-menu-item index="/book">书籍管理</el-menu-item>
-      <el-menu-item index="/order">我的订单</el-menu-item>
-      <el-menu-item index="/news">新闻管理</el-menu-item>
-      <el-menu-item index="/map">百度地图</el-menu-item>
-      <el-menu-item index="/im">聊天室</el-menu-item>
-      <el-menu-item index="/message">在线留言</el-menu-item>
+      <el-menu-item index="/home">
+        <i class="el-icon-house"></i>  主页
+      </el-menu-item>
+      <el-menu-item index="/category">
+        <i class="el-icon-menu"></i> 分类管理
+      </el-menu-item>
+      <el-menu-item index="/book">
+        <i class="el-icon-files"></i> 书籍管理
+      </el-menu-item>
+      <el-menu-item index="/order">
+        <i class="el-icon-s-order"></i> 我的订单
+      </el-menu-item>
+      <el-menu-item index="/news">
+        <i class="el-icon-news"></i> 新闻管理
+      </el-menu-item>
+      <el-menu-item index="/map"><i class="el-icon-map-location"></i> 百度地图</el-menu-item>
+      <el-menu-item index="/im">
+        <i class="el-icon-chat-round"></i> 聊天室
+      </el-menu-item>
+      <el-menu-item index="/message">
+        <i class="el-icon-message"></i> 聊天室在线留言
+      </el-menu-item>
       <el-submenu index="1" v-if="user.role === 1">
-        <template #title>系统管理</template>
-        <el-menu-item index="/user">用户管理</el-menu-item>
+        <template #title>
+          <i class="el-icon-s-tools"></i> 系统管理
+        </template>
+        <el-menu-item index="/user">
+          <i class="el-icon-user"></i> 用户管理
+        </el-menu-item>
       </el-submenu>
-      <el-menu-item index="/donate">请作者喝杯咖啡</el-menu-item>
+      <el-menu-item index="/donate">
+        <i class="el-icon-coffee"></i> 请作者喝杯咖啡
+      </el-menu-item>
     </el-menu>
   </div>
 </template>
