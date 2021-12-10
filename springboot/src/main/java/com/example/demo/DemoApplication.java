94ce0f8151ef2e5aeb19a6aaf1626432d35fcbfd
WhoRun <473189493@qq.com>
2021-12-10 13:35:20 +0800

    feat: 引入webase接口

--- springboot/src/main/java/com/example/demo/DemoApplication.java
+++ springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.webase.transaction.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableWebSecurity   //开启security
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+		TransactionService tranService = applicationContext.getBean(TransactionService.class);
+		tranService.sendTransaction();
+	}
+	@Bean
+	public RestTemplate getRestTemplate() {
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		factory.setReadTimeout(10000);// ms
+		factory.setConnectTimeout(10000);// ms
+		return new RestTemplate(factory);
 	}
 
 }
